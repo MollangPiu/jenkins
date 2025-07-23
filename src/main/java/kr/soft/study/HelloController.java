90cec6dc8761f245e4d96f458f5fd80899ca0d1c
MollangPiu <99593050+MollangPiu@users.noreply.github.com>
2025-07-23 12:40:28 +0900

    public

--- src/main/java/kr/soft/study/HelloController.java
+++ src/main/java/kr/soft/study/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/test")
     public String test() {
-        System.out.println("Test");
+        System.out.println("Test123");
         return "test.html";
     }
 }
