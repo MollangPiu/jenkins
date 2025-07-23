6a4c5ddd77663473dca184f2fbdd07216bf7ab26
MollangPiu <99593050+MollangPiu@users.noreply.github.com>
2025-07-23 12:41:49 +0900

    Feat: default

--- src/main/java/kr/soft/study/HelloController.java
+++ src/main/java/kr/soft/study/HelloController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/test";
+    }
+
     @GetMapping("/test")
     public String test() {
         System.out.println("Test123");
