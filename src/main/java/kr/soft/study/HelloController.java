ee48f1d752b8ddb11abcc8740fd6b0d1a350e874
MHS <hayaruby7909@gmail.com>
2025-07-30 15:23:36 +0900

    test: message

--- src/main/java/kr/soft/study/HelloController.java
+++ src/main/java/kr/soft/study/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @GetMapping("/test")
     public String test() {
 
-        System.out.println("test하기");
+        System.out.println("test하기1");
 
         return "test.html";
     }
